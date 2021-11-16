3dc3d1154126671e713062bddb78a198766df0d2
Brad Dwyer <brad@roboflow.ai>
2021-11-16 08:54:53 -0600

    Cross-build from arm64 to amd64 and vice versa

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "🛠 : Building roboflow/inference-server:cpu"
-docker build -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
+docker buildx build --platform linux/amd64 -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
 
 echo ""
 echo "🛠 : Building roboflow/inference-server:gpu"
-docker build -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
+docker buildx build --platform linux/amd64 -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
 
 echo ""
 echo "🛠 : Building roboflow/inference-server:jetson"
-docker build -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
+docker buildx build --platform linux/arm64 -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
