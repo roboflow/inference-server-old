dbfef2ebe0ca9584d5cc370a5fc66c1e893fbb4d
Spacewaffle <jon.syndicate@gmail.com>
2022-05-31 13:23:43 -0400

    fix parameter references for splitImage

--- server/index.js
+++ server/index.js
@@ -42,9 +42,9 @@ const SERVER_START = Date.now();
 
 //this splits a 3d tensor image into smaller tiles of the image
 //tile size is an array [h, w] of height and width of tile dimensions
-const splitImage = (image, tileSize) => {
-    const imageShape = x.shape;
-    const tileRows = x.reshape([imageShape[0], -1, tileSize[1], imageShape[2]])
+const splitImage = (image3, tileSize) => {
+    const imageShape = image3.shape;
+    const tileRows = image3.reshape([imageShape[0], -1, tileSize[1], imageShape[2]])
     const serialTiles = roboflow.tf.transpose(tileRows, [1, 0, 2, 3])
     return roboflow.tf.reshape(serialTiles, [-1, tileSize[1], tileSize[0], imageShape[2]])
 }
