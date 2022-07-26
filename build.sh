fded89295764a62343f1b59e8ac92b94f0ce322f
Brad Dwyer <brad@roboflow.ai>
2022-07-26 00:28:01 -0500

    WIP - bump roboflow-js versions and add docker cross-platform build

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# needed to build multi-platform images
+docker buildx create --name roboflow
+docker buildx use roboflow
+
 echo "🛠 : Building roboflow/inference-server:cpu"
-docker buildx build --platform linux/amd64 -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
+docker buildx build --platform linux/amd64,linux/arm64 --output=image -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
 
-echo ""
-echo "🛠 : Building roboflow/inference-server:gpu"
-docker buildx build --platform linux/amd64 -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
+# echo ""
+# echo "🛠 : Building roboflow/inference-server:gpu"
+# docker buildx build --platform linux/amd64,linux/arm64 --output=image -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
 
-echo ""
-echo "🛠 : Building roboflow/inference-server:jetson"
-docker buildx build --platform linux/arm64 -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
+# echo ""
+# echo "🛠 : Building roboflow/inference-server:jetson"
+# docker buildx build --platform linux/arm64 --output=image -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "🚀 : Deploying roboflow/inference-server:cpu"
-docker push "roboflow/inference-server:cpu"
+# needed to build multi-platform images
+docker buildx create --name roboflow
+docker buildx use roboflow
 
-echo ""
-echo "🚀 : Deploying roboflow/inference-server:gpu"
-docker push "roboflow/inference-server:gpu"
+echo "🛠 : Building roboflow/inference-server:cpu"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
 
-echo ""
-echo "🚀 : Deploying roboflow/inference-server:jetson"
-docker push "roboflow/inference-server:jetson"
+# echo ""
+# echo "🛠 : Building roboflow/inference-server:gpu"
+# docker buildx build --platform linux/amd64,linux/arm64 --push -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
+
+# echo ""
+# echo "🛠 : Building roboflow/inference-server:jetson"
+# docker buildx build --platform linux/arm64 --push -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
