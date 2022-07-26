94d98c7ea630fc9fdfd1deb3b0bc98249a0cfdcb
Brad Dwyer <brad@roboflow.ai>
2022-07-26 13:23:42 -0500

    Working cross-platform Docker build

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ docker buildx use roboflow
 echo "🛠 : Building roboflow/inference-server:cpu"
 docker buildx build --platform linux/amd64,linux/arm64 --output=image -t "roboflow/inference-server:cpu" -f- . < Dockerfile.cpu
 
-# echo ""
-# echo "🛠 : Building roboflow/inference-server:gpu"
-# docker buildx build --platform linux/amd64,linux/arm64 --output=image -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
+echo ""
+echo "🛠 : Building roboflow/inference-server:gpu"
+docker buildx build --platform linux/amd64,linux/arm64 --output=image -t "roboflow/inference-server:gpu" -f- . < Dockerfile.gpu
 
-# echo ""
-# echo "🛠 : Building roboflow/inference-server:jetson"
-# docker buildx build --platform linux/arm64 --output=image -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
+echo ""
+echo "🛠 : Building roboflow/inference-server:jetson"
+docker buildx build --platform linux/arm64 --output=image -t "roboflow/inference-server:jetson" -f- . < Dockerfile.jetson
